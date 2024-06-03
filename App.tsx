ccd00182ae68035e3a319365ef023911a5bba29d
Giorgi Pirtskhalaishvili <giorgio.p@deazydelivery.co.uk>
2024-06-03 07:02:40 +0400

    chore(upt): update example app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { useCallback, useState } from 'react';
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useCallback, useRef, useState } from 'react';
+import { Alert, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import SwipeableFlatList from 'rn-gesture-swipeable-flatlist';
 
 
@@ -8,93 +8,116 @@ import SwipeableFlatList from 'rn-gesture-swipeable-flatlist';
 
 
 interface DataItem {
-  id: string;
-  text: string;
+   id: string;
+   text: string;
 }
 
-const initialData: DataItem[] = Array.from({length: 20}, (_, i) => ({
-  id: `item-${i}`,
-  text: `Item #${i}`,
+const initialData: DataItem[] = Array.from({ length: 20 }, (_, i) => ({
+   id: `item-${i}`,
+   text: `Item #${i}`,
 }));
 
 
 export default function App() {
 
-  const [data, setData] = useState<DataItem[]>(initialData);
+   const [data, setData] = useState<DataItem[]>(initialData);
+   const flatlistRef = useRef<FlatList<DataItem> | null>(null);
+   const deleteItem = useCallback((id: string) => {
+      setData((prevData) => prevData.filter((item) => item.id !== id));
+   }, []);
 
-  const deleteItem = useCallback((id: string) => {
-     setData((prevData) => prevData.filter((item) => item.id !== id));
-  }, []);
+   const scrollToEnd = () => {
+      flatlistRef.current?.scrollToEnd();
+   }
+   const editItem = useCallback((id: string) => {
+      Alert.alert(`Editing item with id ${id}`)
+   }, []);
 
-  const editItem = useCallback((id: string) => {
-    Alert.alert(`Editing item with id ${id}`)
-  }, []);
+   const renderRightAction = useCallback((item: DataItem) => (
+      <TouchableOpacity onPress={() => deleteItem(item.id)} style={styles.rightAction}>
+         <Text style={styles.actionText}>Delete</Text>
+      </TouchableOpacity>
+   ), [deleteItem]);
 
-  const renderRightAction = useCallback((item: DataItem) => (
-     <TouchableOpacity onPress={() => deleteItem(item.id)} style={styles.rightAction}>
-        <Text style={styles.actionText}>Delete</Text>
-     </TouchableOpacity>
-  ), [deleteItem]);
+   const renderLeftAction = useCallback((item: DataItem) => (
+      <TouchableOpacity onPress={() => editItem(item.id)} style={styles.leftAction}>
+         <Text style={styles.actionText}>Edit</Text>
+      </TouchableOpacity>
+   ), [editItem]);
 
-  const renderLeftAction = useCallback((item: DataItem) => (
-     <TouchableOpacity onPress={() => editItem(item.id)} style={styles.leftAction}>
-        <Text style={styles.actionText}>Edit</Text>
-     </TouchableOpacity>
-  ), [editItem]);
+   const renderItem = useCallback(({ item }: { item: DataItem }) => (
+      <View style={styles.item}>
+         <Text>{item.text}</Text>
+      </View>
+   ), []);
 
-  const renderItem = useCallback(({item}: {item: DataItem}) => (
-        <View style={styles.item}>
-           <Text>{item.text}</Text>
-        </View>
-  ), []);
+  
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <SwipeableFlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        enableOpenMultipleRows={false} //make sure to refresh the list once you alter this
-        renderItem={renderItem}
-        renderLeftActions={renderLeftAction}
-        renderRightActions={renderRightAction}
-      />
-      
-    </View>
-  );
+   return (
+      <View style={styles.container}>
+         <StatusBar style="auto" />
+         <TouchableOpacity style={styles.touchableStyle} onPress={scrollToEnd}>
+            <Text>Scroll to end</Text>
+         </TouchableOpacity>
+         <SwipeableFlatList
+            ref={flatlistRef}
+            data={data}
+            keyExtractor={(item) => item.id}
+            enableOpenMultipleRows={false} //make sure to refresh the list once you alter this
+            renderItem={renderItem}
+            renderLeftActions={renderLeftAction}
+            renderRightActions={renderRightAction}
+         />
+
+      </View>
+   );
 }
 
 
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  item: {
-     padding: 20,
-     backgroundColor: '#fff',
-     borderBottomColor: '#ccc',
-     borderBottomWidth: 1,
-  },
-  rightAction: {
-     backgroundColor: 'red',
-     justifyContent: 'center',
-     alignItems: 'center',
-     width: 100,
-     height: '100%',
-  },
-  leftAction: {
-     backgroundColor: 'lightblue',
-     justifyContent: 'center',
-     alignItems: 'center',
-     width: 100,
-     height: '100%',
-  },
-  actionText: {
-     color: '#fff',
-     fontWeight: '600',
-     padding: 20,
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+   },
+   item: {
+      padding: 20,
+      backgroundColor: '#fff',
+      borderBottomColor: '#ccc',
+      borderBottomWidth: 1,
+   },
+   rightAction: {
+      backgroundColor: 'red',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 100,
+      height: '100%',
+   },
+   leftAction: {
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 100,
+      height: '100%',
+   },
+   actionText: {
+      color: '#fff',
+      fontWeight: '600',
+      padding: 20,
+   },
+   touchableStyle:{
+      backgroundColor: 'red',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+      marginTop: 50,
+      marginBottom:10
+   },
+
+   buttonText: {
+      color: '#fff',
+      fontWeight: 'bold',
+      textAlign: 'center',
+   },
 });
